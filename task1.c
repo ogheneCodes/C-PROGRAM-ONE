42ca17c0ebc2de87155b7672aaebfd292e243e47
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-06-29 20:32:02 +0100

    First Commit for C programming

---
+++ task1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Names of variables
+char name [20];
+int main() {
+  printf("What is your name?  ");
+  scanf("%s", name);
+  printf("\n");
+  printf("My name is %s", name);
+  printf("\n");
+  printf("Are you ready?");
+  printf("\n");
+//   scanf("");
+  printf("Howdy, neighbor! This is my first C program.\n");
+  printf("\n");
+  printf("\n");
+  return 0;
+}
+//This are all written in C using Vim editor
