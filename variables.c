c0bb51beba5c60fd47e07946cf859d3dfbb934cc
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-06-30 13:46:54 +0100

    Variables in C

---
+++ variables.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    // variables in C
+    char name[10] = "maxwell";
+    int number = 22;
+    char letter = 'A';
+    double decimal = 10.1;
+    float decimal2 = 10.2;
+
+    // printf("This is a variable string of text named using char for:  %s", name);
+    printf("\n This is a variable type of number using int for: %d", number);
+    printf("\n This is a variable letter of a single letter using char for: %d", letter);
+    printf("\n This is a variable type of decimal numbers  using double for: %f", decimal);
+    printf("\n This is a variable type of float number using float for: %f", decimal2);
+    printf("\n");
+    printf("\n");
+    /*changing a code */
+    letter = 'B';
+    number = 45;
+    printf("This is a variable string of text named using char for:  %c", letter);
+    printf("\n This is a variable type of number using int for: %d", number);
+    printf("\n");
+    printf("\n");
+
+    return (0);
+}
