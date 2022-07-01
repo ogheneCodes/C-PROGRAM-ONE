47c8261038dfb03e159cea66a767ef245d5849c4
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-07-01 23:17:54 +0100

    if statment

---
+++ ifElse.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* A voting system
+
+
+*/
+
+int main()
+{
+    char name[10];
+    int age;
+    {
+        printf("What is your name: ");
+        scanf("%s", &name);
+        printf("\n");
+        printf("Whats is your Age: ");
+        scanf("%d", &age);
+        printf("\n");
+
+        if(age < 18){
+            printf("Hello %s, thank you for coming out but you are not eligible to vote becuase  you are %d years old", name, age);
+        }else{
+            printf("Hello %s, thank you for coming out to vote as you are eligible to vote since you are %d years old", name, age);
+        }
+          printf("\n");
+        return 0;
+    }
+}
