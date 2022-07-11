48859b3f1937e43b648c44722f5278c89f0fd69d
ogheneCodes <oghenemaga.ebobo@unn.edu.ng>
2022-07-11 20:15:27 +0100

    preprocessor defined operator

---
+++ C 24/PRE _PROCESSOR/processor2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+The preprocessor defined operator is used in 
+constant expressions to determine if an identifier is 
+defined using #define. If the specified identifier is defined, 
+the value is true (non-zero). If the symbol is not defined,
+the value is false (zero). 
+*/
+
+
+#if !defined (MESSAGE)
+   #define MESSAGE "You wish!"
+#endif
+
+int main(void) {
+   printf("Here is the message: %s\n", MESSAGE);  
+   return 0;
+}
